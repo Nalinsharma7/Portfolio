5ebee3b116afe365f9509c7c4f5be871706cdea3
Nalinsharma7 <nalinsharma606@gmail.com>
2022-10-25 16:55:52 +0530

    Animation

--- src/App.js
+++ src/App.js
@@ -1,69 +1,14 @@
 import './App.css';
 import Navigation from './screens/navigationBar/navigationBar'
-import Particles from 'react-tsparticles'
+// import Particles from 'react-tsparticles'
+import Particles from './Particles';
 
 function App() {
   return (
     <div className="App">
       <Navigation />
-      <Particles 
-        id="tsparticles"
-        options={{
-          background: {
-            color: {
-              value: "#212529",
-            },
-          },
-          fpsLimit: 60,
-          interactivity: {
-            events: {
-              onClick: {
-                enable: true,
-                mode: "push",
-              },
-              // onHover: {
-              //   enable: true,
-              //   mode: "repulse",
-              // },
-              resize: true,
-            },
-            modes: {
-              bubble: {
-                distance: 200,
-                duration: 2,
-                opacity: 0.8,
-                size: 40,
-              },
-              push: {
-                quantity: 4,
-              },
-              repulse: {
-                distance: 200,
-                duration: 0.4,
-              },
-            },
-          },
-          particles: {
-            color: {
-              value: "#ffffff",
-            },
-            links: {
-              color: "#ffff5f",
-              distance: 150,
-              enable: true,
-              opacity: 0.5,
-            },
-            move: {
-              direction: "none",
-              enable: true,
-              outMode: "bounce",
-              random: false,
-              speed: 1,
-              straight: false,
-            },
-          },
-        }}
-      />
+      <Particles />
+      
         
     </div>
   );
