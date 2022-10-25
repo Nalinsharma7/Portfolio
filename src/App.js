1bc23ea24de04e22b049c8e38240733d46218305
Nalinsharma7 <nalinsharma606@gmail.com>
2022-10-26 00:30:56 +0530

    Technologies

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Navigation from './screens/navigationBar/navigationBar'
 import Particles from './Particles';
 import Header from './screens/header/Header';
 import AboutMe from './screens/aboutMe/AboutMe';
+import Technologies from './screens/technologies/Technologies';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Header />
       <Particles />
       <AboutMe />
+      <Technologies />
       
         
     </div>
